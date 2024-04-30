3dd1f563c185fa901146f95ba88ef35cdf146c38
Mobinadgi <mobinadehghani84@gmail.com>
2024-05-01 00:44:16 +0330

    Handled the section of adding and showing students

--- googleForm.cpp
+++ googleForm.cpp
@@ -20,6 +20,8 @@ char googleFormDashboard() {
 int main() {
     do {
         char cmd = googleFormDashboard();
+        fstream studentFile = fstream("students.txt");
+        fstream examFile = fstream("exams.txt");
         if (cmd == '1') {
             // Add new exam
         } else if (cmd == '2') {
@@ -27,14 +29,49 @@ int main() {
         } else if (cmd == '3') {
             // Exams pending to be marked
         } else if (cmd == '4') {
-            // List of the current students
+            string ID, name, email;
+            while (studentFile >> ID >> name >> email) {
+                cout << "StudentID: " << ID << " ___Name: " << name << " ___Email: " << email << endl;
+            }
         } else if (cmd == '5') {
-            // Add new students to the list
+            cout << "1. Add a student from console." << endl;
+            cout << "2. Add list of students from a file." << endl;
+            cout << "Enter a command (number): ";
+            cin >> cmd;
+            if (cmd == '1') {
+                string ID, name, email;
+                cout << "Enter StudentID: ";
+                cin >> ID;
+                cout << "Enter Name: ";
+                cin >> name;
+                cout << "Enter Email: ";
+                cin >> email;
+                studentFile << ID << " " << name << " " << email << endl;
+            }
+            if (cmd == '2') {
+                string filePath;
+                cout << "Enter file path (note that in that file students must be like this: StudentID Name Email, if the format is not met the program won't work properly and everything will break.): ";
+                cin >> filePath;
+                ifstream file(filePath);
+                if (file.is_open()) {
+                    string ID, name, email;
+                    while (file >> ID >> name >> email) {
+                        studentFile << ID << " " << name << " " << email << endl;
+                    }
+                    file.close();
+                }
+                else {
+                    cout << "File not found. Please try again." << endl;
+                }
+            }
+            else {
+                cout << "Invalid command. Please try again." << endl;
+            }
         } else if (cmd == '6') {
-            // Exit
+            cout << "Thank you for chosing out service. Good bye!" << endl;
             break;
         } else {
-            
+            cout << "Invalid command. Please try again." << endl;
         }
     } while (true); 
 }
